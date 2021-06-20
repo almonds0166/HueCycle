5ca5562ca2eae409a4533738abce1c95ec2a9c6c
Madison Landry <mlandry@mit.edu>
2021-06-20 13:12:46 -0700

    Upload code
    * Add README.md
    * Add .gitignore
    * Add huecycle.py
    * Add input.png
    * Add output.gif
    * Add requirements.txt

---
+++ README.md
@@ -0,0 +1,34 @@
+# HueCycle
+
+Just a Python script to convert a PNG into a GIF by rotating it around the hue spectrum.
+
+Doesn't work with transparency... (yet..?)
+
+## Command-line args
+
+|  Argument  | Arg  |                                                              |
+| :--------: | :--: | ------------------------------------------------------------ |
+|  `--help`  | `-h` | Prints help leaflet, then exits.                             |
+| `--input`  | `-i` | Path to the input PNG. (required)                            |
+| `--output` | `-o` | Path to the output GIF. (required)                           |
+| `--period` | `-p` | The number of seconds for the image to make a 360 degree hue cycle. Default 10. |
+|  `--step`  | `-s` | The number of degrees to increment the picture between GIF frames. Default 5. |
+
+## Example usage
+
+**`input.png`**:
+
+<div align="center"><img src="./input.png" width="50%"></div>
+
+**Command**:
+
+```bash
+python huecycle.py -i input.png -o output.gif
+```
+
+**`output.gif`**:
+
+<div align="center"><img src="./output.gif" width="50%"></div>
+
+
+
---
+++ huecycle.py
@@ -0,0 +1,95 @@
+
+import argparse
+from pathlib import Path
+import math
+import colorsys
+from typing import Union
+
+from PIL import Image, ImageColor
+import numpy as np
+
+class HueCycler:
+   """Class that processes the command-line arguments and renders the GIF.
+   """
+   def __init__(self):
+      self.parser = argparse.ArgumentParser(
+         description="Turn a .PNG into a .GIF that cycles around the hue spectrum."
+      )
+
+      self.parser.add_argument(
+         "-i", "--input",
+         metavar="PATH",
+         type=Path,
+         help="Path to the input PNG."
+      )
+      self.parser.add_argument(
+         "-o", "--output",
+         metavar="PATH",
+         type=Path,
+         help="Path to the output GIF."
+      )
+      self.parser.add_argument(
+         "-p", "--period",
+         metavar="N",
+         type=int,
+         default=10,
+         help="The number of seconds for the image to make a 360 degree hue cycle. Default 10."
+      )
+      self.parser.add_argument(
+         "-s", "--step",
+         metavar="N",
+         type=int,
+         default=5,
+         help="The number of degrees to increment the picture between GIF frames. Default 5."
+      )
+
+      self._parse_args()
+
+   def _parse_args(self):
+      """Parse the command-line arguments.
+      """
+      self.args = self.parser.parse_args()
+      
+      self.input_file = Path(self.args.input)
+      self.output_file = Path(self.args.output)
+      self.period = self.args.period
+      self.step = self.args.step
+      self.ms = math.ceil(self.step * self.period / 360)
+      self.num_frames = math.ceil(360 / self.step)
+
+      self.input_image = Image.open(self.input_file).convert("RGBA")
+
+   def process(self):
+      """Render the GIF!
+      """
+      rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
+      hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
+
+      arr = np.array(np.asarray(self.input_image).astype("float"))
+      r, g, b, a = np.rollaxis(arr, axis=-1)
+      h, s, v = rgb_to_hsv(r, g, b)
+
+      frames = []
+      for i in range(1, self.num_frames):
+         shift = (i * self.step) / 360
+         h_ = (h + shift) % 1.0
+         r_, g_, b_ = hsv_to_rgb(h_, s, v)
+         arr_ = np.dstack((r_, g_, b_, a))
+         frame = Image.fromarray(arr_.astype("uint8"), "RGBA")
+         frames.append(frame)
+
+      self.input_image.save(
+         self.output_file,
+         **self.input_image.info,
+         save_all=True,
+         append_images=frames,
+         optimize=True,
+         duration=self.ms,
+         loop=0
+      )
+
+if __name__ == "__main__":
+   hc = HueCycler()
+   hc.process()
+
+
